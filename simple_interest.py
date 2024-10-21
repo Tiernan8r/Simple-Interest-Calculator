3b8440ada35e40cfa061355bcc46aa0cb5f291f8
Tiernan8r <tiernan8r@pm.me>
2024-10-21 14:00:09 +0100

    LoanData str() formatting

--- simple_interest.py
+++ simple_interest.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import List, Tuple
 
+import art
+import prettytable
+
+
 @dataclass
 class LoanData:
     start_date: datetime
@@ -15,6 +19,19 @@ class LoanData:
     @property
     def total_interest_rate(self):
         return self.base_interest_rate + self.margin
+
+    def _str_start_time(self) -> str:
+        return datetime.strftime(self.start_date, "%d/%m/%Y")
+
+    def _str_end_time(self) -> str:
+        return datetime.strftime(self.end_date, "%d/%m/%Y")
+
+    # As a oneliner
+    def __str__(self) -> str:
+
+        return f"{self.loan_currency_str}{self.loan_amount:.2f} \
+            @ {self.total_interest_rate}% ({self.base_interest_rate}% + {self.margin}%) \
+                from {self._str_start_time()} to {self._str_end_time()}"
 
 class Application:
 
